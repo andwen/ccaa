d3e00e7d37a92636a137fc562d88d52481744a51
andwen <16484065+andwen@users.noreply.github.com>
2025-03-02 18:17:38 +0800

    Update upbt.sh

--- ccaa_dir/upbt.sh
+++ ccaa_dir/upbt.sh
@@ -1,24 +1,24 @@
 
-
-#导入环境变量
+#import environment variable
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/bin:/sbin
 export PATH
 
 function up_tracker(){
-	#下载最新的bt-tracker
-	wget -O /tmp/trackers_best.txt https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt
-	tracker=$(cat /tmp/trackers_best.txt)
-	#替换处理bt-tracker
-	tracker="bt-tracker="${tracker}
-	#更新aria2配置
-	sed -i '/bt-tracker.*/'d /etc/ccaa/aria2.conf
-	echo ${tracker} >> /etc/ccaa/aria2.conf
-	echo '-------------------------------------'
-	echo 'bt-tracker update completed.'
-	echo '-------------------------------------'
+        #download bt-tracker_best
+        wget -O /tmp/trackers_best.txt https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt
+        #add comma
+        sed 's/$/,/' /tmp/trackers_best.txt >/tmp/trackers_best.txt
+        tracker=$(cat /tmp/trackers_best.txt)
+        #replace bt-tracker
+        tracker="bt-tracker="${tracker}
+        #update aria2.conf
+        sed -i '/bt-tracker.*/'d /etc/ccaa/aria2.conf
+        echo ${tracker} >> /etc/ccaa/aria2.conf
+        echo '-------------------------------------'
+        echo 'bt-tracker update completed.'
+        echo '-------------------------------------'
 }
 
 up_tracker
 
-#重启服务
-/usr/sbin/ccaa restart
+#restart
